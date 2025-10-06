8f62e7a2c780ac98b058a6e539e6b5fe7b85463f
Gio Gasta <10934056@itisrossi.vi.it>
2025-10-06 16:12:29 +0200

    updated Pizza.java (constructor)

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,8 +1,17 @@
 public class Pizza
 {
+    public String _id;
     public String nome;
     public String ingredienti;
-    float prezzo;
+    public float prezzo;
+
+    public Pizza() {}
+
+    public Pizza(String nome, String ingredienti, float prezzo) {
+        this.nome = nome;
+        this.ingredienti = ingredienti;
+        this.prezzo = prezzo;
+    }
 
     @Override
     public String toString(){
